83629692840df8a35ddb687ce18fe25b0d9ff8e5
encangwahyudin25-pixel <encangwahyudin25@gmail.com>
2025-12-31 17:36:17 +0700

    Update telegram_config.py

--- config/telegram_config.py
+++ config/telegram_config.py
@@ -1,4 +1,4 @@
 import os
 
-BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
