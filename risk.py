e3fd9f22bccb590f918eccd42bb97fdaa3c8dbfa
encangwahyudin25-pixel <encangwahyudin25@gmail.com>
2025-12-31 16:24:43 +0700

    Update risk.py

--- risk.py
+++ risk.py
@@ -1,15 +1,8 @@
 def risk_levels(entry, atr, direction):
     if direction == "LONG":
-        sl = entry - atr * 1.5
-        tp1 = entry + atr * 1.5
-        tp2 = entry + atr * 3
-        tp3 = entry + atr * 4.5
-        tp4 = entry + atr * 6
+        sl = entry - atr*1.5
+        tps = [entry + atr*i for i in [1.5,3,4.5,6]]
     else:
-        sl = entry + atr * 1.5
-        tp1 = entry - atr * 1.5
-        tp2 = entry - atr * 3
-        tp3 = entry - atr * 4.5
-        tp4 = entry - atr * 6
-
-    return sl, tp1, tp2, tp3, tp4
+        sl = entry + atr*1.5
+        tps = [entry - atr*i for i in [1.5,3,4.5,6]]
+    return sl, tps
